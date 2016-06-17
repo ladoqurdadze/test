349cefdc0bd53de6a6de19a2dad783d543f41bb2
lado <lado_qurdadze@mail.ru>
2016-06-18 02:06:32 +0300

    master commmietttttttttttttt

--- ladosweb/index.php
+++ ladosweb/index.php
@@ -1,5 +1,12 @@
 <?php
+
+
 /**
+
+  Branch  master ---------->  cvlileba master -s ganshtoebashi !!! 
+  
+
+
  * The directory in which your application specific resources are located.
  * The application directory must contain the bootstrap.php file.
  *
